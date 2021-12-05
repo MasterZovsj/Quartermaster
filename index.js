9870d2cbaad397e45a690ef399bae2eb6453bdc0
Micah Fierro <44856746+MasterZovsj@users.noreply.github.com>
2021-12-05 11:42:57 -0500

    Update index.js
    clean up useless functions

--- index.js
+++ index.js
@@ -27,20 +27,6 @@ for (const file of commandFiles) {
   // With the key as the command name and the value as the exported module
   client.commands.set(command.data.name, command)
 }
-/*
-client.on('guildMemberUpdate', (oldMember, newMember) => {
-	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
-	const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-	if (removedRoles.size > 0) {
-		console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
-	}
 
-	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
-	const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
-	if (addedRoles.size > 0) {
-		console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
-	}
-});
-*/
 // Login to Discord with your client's token
 client.login(token);
