dddab2f8febc3d0c3fe13f1d6ec39485c9223538
Micah Fierro <44856746+MasterZovsj@users.noreply.github.com>
2021-12-05 11:42:45 -0500

    Update roleResponse.js
    handle selection based on if user wants role removed or added

--- events/roleResponse.js
+++ events/roleResponse.js
@@ -2,10 +2,18 @@ module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
   if (!interaction.isSelectMenu()) return;
-	interaction.member.roles.add(interaction.values)
-	await interaction.update({ content: `You have been given the ${interaction.guild.roles.resolve(...interaction.values)} role!`, components: [] })
-	const role = interaction.guild.roles.resolve(...interaction.values)
-	console.log(role.name);
+	if (interaction.customId == 'addRole'){
+		interaction.member.roles.add(interaction.values)
+		await interaction.update({ content: `You have been given the ${interaction.guild.roles.resolve(...interaction.values)} role!`, components: [] })
+		const role = interaction.guild.roles.resolve(...interaction.values)
+		console.log(`Added ${role.name} to ${interaction.member.displayName}`);
+
+	} else if (interaction.customId == 'removeRole') {
+		interaction.member.roles.remove(interaction.values)
+		await interaction.update({ content: `${interaction.guild.roles.resolve(...interaction.values)} has been removed.`, components: [] })
+		const role = interaction.guild.roles.resolve(...interaction.values)
+		console.log(`Removed ${role.name} to ${interaction.member.displayName}`);
+	}
 
 
 
